b248764978bdbe6c6b953b0260739892c4a67642
omnitechphilippines <omnitechphilippines@users.noreply.github.com>
2025-10-17 17:57:30 +0800

    Initial Node-RED setup

---
+++ server.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const RED = require("node-red");
+
+const express = require("express");
+const app = express();
+
+// Render provides the port via environment variable
+const port = process.env.PORT || 1880;
+
+// Create a server
+const server = http.createServer(app);
+
+// Node-RED settings
+const settings = {
+  adminAuth: {
+    type: "credentials",
+    users: [{
+        username: "admin",
+        password: "$2y$08$SMI8G0QUmsJO.SB3OFPaPOvjDnkhw9aJ0EH75gFcJrkUnQK/n.Fd.",
+        permissions: "*"
+    }]
+  },
+  httpNodeAuth: {user:"$2y$08$SMI8G0QUmsJO.SB3OFPaPOvjDnkhw9aJ0EH75gFcJrkUnQK/n.Fd."},
+};
+
+// Initialize Node-RED
+RED.init(server, settings);
+
+// Start the server
+server.listen(port, () => {
+  console.log(`Node-RED running on port ${port}`);
+});
+
+// Start Node-RED runtime
+RED.start();
